2431bee4ffd1ad4f736633dab6e744a194ffb8f1
Nassim069 <58385781+Nassim069@users.noreply.github.com>
2020-03-18 17:38:10 +0100

    Update responsable.cs

--- Classes/responsable.cs
+++ Classes/responsable.cs
@@ -1,13 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 public class responsable
 {
     public static List<Employé> liste_employes = new List<Employé>();
     public static List<Type_pret> liste_types = new List<Type_pret>();
     public static List<Archive> liste_archives = new List<Archive>();
+    public static List<Pret_remboursable> liste_pret_remboursable = new List<Pret_remboursable>();
+    public static List<Pret_non_remboursable> liste_pret_Non_Remboursables = new List<Pret_non_remboursable>();
 
 }
